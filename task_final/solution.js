4b272b57ae6f50ea143d21ddffc4e94e23124351
Anna <zueva_aa@mail.ru>
2023-05-16 12:34:11 +0300

    Task_final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,12 +34,12 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {"client": {name, phone}, "order": {"address": address, "sum": sum}, "goods":[]};
+    let data = {"client": {}, "order": {"address": {}, "sum": {}}, "goods":[]};
     
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i += 1) {
-        data.goods.push({title: goods[i].title, count: goods[i].count});
+        data.goods.push({"title": goods[i].title, "count": goods[i].count});
     }
 
     data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв. " + address.flat;
